439b7b26ff109574da9e89bc58e56f49a83e4404
raidentek <33987212+raidentek@users.noreply.github.com>
2021-03-10 19:19:49 -0500

    red

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import AuthState from "./context/auth/authState";
+import GlobalStyle from "./styles/globalStyled";
 
 ReactDOM.render(
   <React.Fragment>
+    <GlobalStyle/>
     <AuthState>
       <App />
     </AuthState>
